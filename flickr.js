904edb4f9707793d233755fa110f83fef975639b
sindystar <hasungah@gmail.com>
2023-05-15 11:03:31 +0900

    동적 dom형성 / 미리보기 이미지 깨짐

--- flickr.js
+++ flickr.js
@@ -7,6 +7,7 @@ const method = "flickr.interestingness.getList";
 const key = "ebb580daf8a6e1fcec5b215244ed1d71";
 const per_page = 500;
 const format = "json";
+const frame = document.querySelector("#list");
 
 const url = `${base}method=${method}&api_key=${key}&per_page=${per_page}&format=${format}&nojsoncallback=1`;
 
@@ -22,5 +23,29 @@ fetch(url)
   .then((json) => {
     //반환된 값을 json이라는 매개변수로 받은뒤
     let items = json.photos.photo; //콘솔에서 본것처럼 그안의 photos 안의 photo로 접근함
-    console.log(items) ; //500장의 사진이 json객체 배열로
+    // console.log(items) ; //500장의 사진이 json객체 배열로
+
+    let htmls = "";
+
+    items.map((el) => {
+      console.log(el);
+
+      let imgSrc = `http://live.staticflicker.com/${el.server}/${el.id}_${el.secret}_m.jpg`;
+      //이미지의 썸네일 url주소
+      let imgSrcBig = `https://live.staticflickr.com/${el.server}/${el.id}_${el.secret}_b.jpg`;
+      //큰이미지 주소
+
+      htmls +=`
+        <li class="item">
+          <div>
+            <a href=${imgSrcBig}>
+              <img src=${imgSrc}>
+            </a>
+            <p>${el.title}</a>
+          </div>
+        </li>
+      `;
+    })
+
+    frame.innerHTML = htmls;
   })
